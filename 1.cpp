fd74baf7e84b7a4502ac2acd1117ee0143ea9d37
MerlinGuoCL <578975824@qq.com>
2022-10-23 14:58:40 +0800

    test

---
+++ 1.cpp
@@ -0,0 +1,12 @@
+/*#########################################################################
+# File Name: 1.cpp
+# Author:Merlin
+# Created Time: Sun 23 Oct 2022 02:52:27 PM CST
+#########################################################################*/
+#include <iostream>
+using namespace std;
+int main (void)
+{
+  cout << "hello" << endl;
+  return 0;
+}
